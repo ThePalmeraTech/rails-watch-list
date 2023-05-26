9452314f071147279066b719b08f7589548548e0
Hanaley Mosley <hanaleymosley@palmeratech.com>
2023-05-26 05:38:21 -0500

    Solution

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,7 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
-  
+
   def new
     @bookmark = Bookmark.new
   end
@@ -34,5 +34,4 @@ class BookmarksController < ApplicationController
   def set_list
     @list = List.find(params[:list_id])
   end
-
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
-    @movie = Movie.find(params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new
   end
 
   def new
@@ -21,25 +23,18 @@ class ListsController < ApplicationController
     end
   end
 
-  def edit
-    @list = List.find(params[:id])
-  end
-
-  def update
-    @list = List.find(params[:id])
-    @list.update(params[:id])
-
-    redirect_to list_path(@list)
-  end
-
   def destroy
     @list.destroy
-    redirect_to lists_path(@lists)
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
